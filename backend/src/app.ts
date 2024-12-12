f41f55e7a3855f5770b77d2626f09a8183c6690f
Jeison Hurtado <jeisongamersoto@gmail.com>
2024-12-12 18:35:58 -0500

    Ultima actualizacion backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes';
-import clientRoutes from './routes/clientRoutes';
+import userRoutes from './routes/userRoutes';
 import productRoutes from './routes/productRoutes';
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/category', categoryRoutes);
-app.use('/api/client', clientRoutes);
+app.use('/api/user', userRoutes);
 app.use('/api/product', productRoutes)
 
 export default app;
