a6d1dd789b5ce3de2041cde558ca6862b05ee864
Jeison Hurtado <jeisongamersoto@gmail.com>
2024-12-12 17:47:02 -0500

    Cambios paginas

--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -1,7 +1,6 @@
 import Home from '../pages/Home';
 import AddRecipe from '../pages/AddRecipe';
 import RecipeDetails from '../pages/RecipeDetails';
-import Favorites from '../pages/Favorites';
 import RegisterUser from '../pages/RegisterUser';
 import UsersList from '../pages/UsersList';
 
@@ -14,7 +13,6 @@ const routes: Route[] = [
   { path: '/', component: Home },
   { path: '/add-recipe', component: AddRecipe },
   { path: '/recipe/:id', component: RecipeDetails },
-  { path: '/favorites', component: Favorites },
   { path: '/register-user', component: RegisterUser },
   { path: '/users', component: UsersList },
 ];
